722bfcc0f2e33c1c42912dc957c9f2d1954c8c32
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-05-15 10:43:59 -0400

    added basic movement and gravity

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.8f;
+
+    Vector3 velocity;
+
+    public bool IsGrounded() => controller.isGrounded;
+
+    void Update()
+    {
+        if(IsGrounded() && velocity.y < 0)
+        {
+            velocity.y = -5f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed  * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
