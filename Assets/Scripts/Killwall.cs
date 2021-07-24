d7e8155ba01cf66017241adc959fa6616af8c85f
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-07-24 17:39:58 -0400

    Fleshed out Level 9.  Added killwalls.  Added new materials

---
+++ Assets/Scripts/Killwall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Killwall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
