ccdac3c01b30bdf45e898ca69f22e772404a7bd9
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-08-06 09:49:27 -0400

    Added Game Manager and a timer.  Fixed Level 9

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time - startTime;
+        string minutes = ((int)time / 60).ToString("00");
+        string seconds = (time % 60).ToString("00.00");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
