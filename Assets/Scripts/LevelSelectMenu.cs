7672a7cc2f590565d8c485b4f2709579aa122b4d
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-08-06 11:34:08 -0400

    Added Main Menu with level select

---
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads Level 1
+    /// </summary>
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    /// <summary>
+    /// Loads Level 2
+    /// </summary>
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    /// <summary>
+    /// Loads Level 3
+    /// </summary>
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    /// <summary>
+    /// Loads Level 1
+    /// </summary>
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+
+    /// <summary>
+    /// Loads Level 5
+    /// </summary>
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+
+    /// <summary>
+    /// Loads Level 6
+    /// </summary>
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene("Level 6");
+    }
+
+    /// <summary>
+    /// Loads Level 7
+    /// </summary>
+    public void LoadLevel7()
+    {
+        SceneManager.LoadScene("Level 7");
+    }
+
+    /// <summary>
+    /// Loads Level 8
+    /// </summary>
+    public void LoadLevel8()
+    {
+        SceneManager.LoadScene("Level 8");
+    }
+
+    /// <summary>
+    /// Loads Level 9
+    /// </summary>
+    public void LoadLevel9()
+    {
+        SceneManager.LoadScene("Level 9");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Exits the game from the Main Menu
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
