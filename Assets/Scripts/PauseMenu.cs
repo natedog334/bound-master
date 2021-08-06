d3da422457f9d9e841ee722aa2066e6b8e17d72a
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-08-06 15:14:51 -0400

    Parially implemented pause menu.  Began working on Level 11

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused;
+
+    
+
+    [SerializeField] GameObject pauseMenu;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode pauseKey = KeyCode.Tab;
+
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
